966934814143a6f25c3160e8f8ff3737877f847d
lhernandez <lhernandez@axosnet.com>
2019-04-06 17:07:30 -0600

    Doble Destructuring

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import WeatherTemperature from './WeatherTemperature';
 import WeatherExtraInfo from './WeatherExtraInfo';
-import { SUNNY, CLOUD } from './../../../constants/weathers';
+
 import './style.css';
 
-const WeatherData = () => (
+const WeatherData = ({data:{temperature, weatherState, humidity, wind}}) => (
     <div className="wetherDataCont">
-        <WeatherTemperature temperature={31} weatherState={SUNNY} />
-        <WeatherExtraInfo humidity={80} wind={"10 m/s"} />
+        <WeatherTemperature temperature={temperature} weatherState={weatherState} />
+        <WeatherExtraInfo humidity={humidity} wind={wind} />
     </div>
 );
 export default WeatherData;
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import WeatherData from './WeatherData';
 import Location from './Location';
 import './style.css';
+import { SUNNY, CLOUD } from './../../constants/weathers';
+
+const data = {
+  temperature: 15,
+  weatherState: SUNNY,
+  humidity: 30,
+  wind: "10 m/s"
+}
+
 const WeatherLocation = () => {
   return (
     <div className="weatherLocationCont">
     <Location city={"Monterrey"}></Location>
-    <WeatherData />
+    <WeatherData data={data} />
     </div>
   )
 }
